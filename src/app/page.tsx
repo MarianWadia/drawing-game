e382609abf88d7a6a8542bd281bdc1009d715f16
MarianIsrael <marianwadia55@gmail.com>
2024-08-30 01:00:27 +0200

    added function of drawing to cnavas while the mouse is down

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
+"use client";
 import useDraw from "@/hooks/useDraw";
-import Image from "next/image";
 
 export default function Home() {
-	const { canvasRef } = useDraw();
+	const { canvasRef, onMouseDown } = useDraw(drawLine);
+
+	function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
+		const lineColor = "#00000";
+		const lineWidth = 5;
+		let startPoint = prevPoint ?? currentPoint;
+		ctx.beginPath();
+		ctx.lineWidth = lineWidth;
+		ctx.strokeStyle = lineColor;
+		ctx.moveTo(startPoint.x, startPoint.y);
+		ctx.lineTo(currentPoint.x, currentPoint.y);
+		ctx.stroke();
+		ctx.fillStyle = lineColor;
+		ctx.beginPath();
+		ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+		ctx.fill();
+	}
 	return (
-		<main className="w-screen h-screen overflow-hidden">
+		<main className="w-screen h-screen overflow-hidden bg-white text-black">
 			<div className="h-full w-full flex flex-row items-center justify-center gap-x-32">
 				<button type="button">Reset board</button>
 				<canvas
-					width={750}
-					height={750}
+					ref={canvasRef}
+					width={500}
+					height={500}
 					className="border border-black rounded-md"
+					onMouseDown={onMouseDown}
 				/>
 			</div>
 		</main>
--- src/hooks/useDraw.ts
+++ src/hooks/useDraw.ts
@@ -1,11 +1,46 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
-function useDraw() {
-    const canvasRef = React.useRef<HTMLCanvasElement>(null)
-    useEffect(()=>{},[])
-    return {
-        canvasRef
-    }
+function useDraw(onDraw: ({ ctx, prevPoint, currentPoint }: Draw) => void) {
+	const canvasRef = React.useRef<HTMLCanvasElement>(null);
+	const prevPoint = useRef<null | Point>(null);
+	const [mouseDown, setMouseDown] = useState(false);
+	const onMouseDown = () => setMouseDown(true);
+	useEffect(() => {
+		let currentRef = canvasRef.current;
+		const handleMouseMove = (e: MouseEvent) => {
+			if(!mouseDown) return
+			console.log({ x: e.clientX, y: e.clientY });
+			const currentPoint = computePointInCanvas(e);
+			const ctx = currentRef?.getContext("2d");
+			if (!ctx || !currentPoint) return;
+			onDraw({ ctx, currentPoint, prevPoint: prevPoint.current });
+			prevPoint.current = currentPoint;
+		};
+
+		const computePointInCanvas = (e: MouseEvent) => {
+			const canvas = canvasRef.current;
+			if (!canvas) return;
+			const rect = canvas.getBoundingClientRect();
+			const x = e.clientX - rect.left;
+			const y = e.clientY - rect.top;
+			return { x, y };
+		};
+		const mouseUpHandler = () => {
+			setMouseDown(false);
+			prevPoint.current = null;
+		};
+		// The event is already passed automatically either we put it like this or we removed it at all and just called the function callback handleMouseMove
+		currentRef?.addEventListener("mousemove", handleMouseMove);
+		window.addEventListener("mouseup", mouseUpHandler);
+		return () => {
+			currentRef?.removeEventListener("mousemove", handleMouseMove);
+			window.removeEventListener("mouseup", mouseUpHandler);
+		};
+	}, [onDraw, mouseDown]);
+	return {
+		canvasRef,
+		onMouseDown,
+	};
 }
 
-export default useDraw
+export default useDraw;
